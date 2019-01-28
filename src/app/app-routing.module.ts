c662c3e7743938ab154e37f2be26c9efb32d3f72
tieng7ng <tieng@laposte.net>
2019-01-28 07:45:10 +0100

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,30 +3,27 @@ import { Routes, RouterModule } from '@angular/router';
 
 //=====
 // components
-import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { SingleBookComponent } from './book-list/single-book/single-book.component';
 import { BookFormComponent } from './book-list/book-form/book-form.component';
-import { HeaderComponent } from './header/header.component';
 // components
 //=====
 
 //=====
 // services
 import { AuthGuardService } from './services/auth-guard.service';
-import { AuthService } from './services/auth.service';
-
 // services
 //=====
+
 const routes: Routes = [
   { path: 'auth/signup', component: SignupComponent },
   { path: 'auth/signin', component: SigninComponent },
-  { path: 'books', component: BookListComponent/*, canActivate: [AuthGuardService]*/ },
-  { path: 'books/new', component: BookFormComponent/*, canActivate: [AuthGuardService]*/ },
+  { path: 'books', component: BookListComponent, canActivate: [AuthGuardService] },
+  { path: 'books/new', component: BookFormComponent, canActivate: [AuthGuardService] },
   { path: 'books/view/:id', component: SingleBookComponent, canActivate: [AuthGuardService] },
-  { path: 'books/edit/:id', component: BookFormComponent/*, canActivate: [AuthGuardService]*/ },
+  { path: 'books/edit/:id', component: BookFormComponent, canActivate: [AuthGuardService] },
   { path: '', redirectTo: 'books', pathMatch: 'full' },
   { path: '**', redirectTo: 'books' }
 ];
